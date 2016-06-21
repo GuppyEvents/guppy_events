c2a13e5cc14df30e40609358cc8cf2983803bb74
kemalsami <kemalsami.karaca@tubitak.gov.tr>
2016-06-21 16:33:01 +0300

    ** Guppy-ADMIN-2-Admin_Panel ** pages are seperated to header, sidebar and footer v.1.3

---
+++ events/src/AppBundle/Form/UniversityType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class UniversityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('link', TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\University',
+        ));
+    }
+}
