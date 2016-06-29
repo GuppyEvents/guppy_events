ae17c8d6c23ef75b8917cc4f1353dbcb80b00848
kemalsami <kemalsami.karaca@tubitak.gov.tr>
2016-06-29 15:02:40 +0300

    ** Guppy-ADMIN-5-Assing admin to clubs ** university add mail server feature implemented

--- events/src/AppBundle/Controller/UniversityController.php
+++ events/src/AppBundle/Controller/UniversityController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\City;
 use AppBundle\Entity\Result;
 use AppBundle\Entity\Address;
 use AppBundle\Entity\University;
+use AppBundle\Entity\UniversityMail;
 use AppBundle\Form\UniversityType;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
@@ -44,51 +45,6 @@ class UniversityController extends Controller
                 'universities'=>$universities
             )
         );
-    }
-
-
-    /**
-     * @Route("/borough-list", name="university_borough_list")
-     * @Security("has_role('ROLE_USER')")
-     */
-    public function getBoroughAction(Request $request)
-    {
-
-        // -- 1 -- Initialization
-            $response = new Response();
-            $response->headers->set('Content-Type', 'application/json');
-
-        // -- 2 -- Try to Get Borough List
-            try{
-
-            // -- 2.1 -- Get borough list
-                $city_id = $request->get('cid');
-                $boroughList = $this->getDoctrine()
-                    ->getRepository('AppBundle:Borough')
-                    ->findBy(array('cityId'=>$city_id));
-
-                $boroughResponse = array();
-                for($i=0; $i<count($boroughList);$i++){
-                    $borough['borough_name'] = $boroughList[$i]->getName();
-                    $borough['borough_id'] = $boroughList[$i]->getId();
-                    array_push($boroughResponse,$borough);
-                }
-
-            // -- 2.1 -- Return Result
-                $response->setContent(json_encode(Result::$SUCCESS->setContent( $boroughResponse )));
-                return $response;
-
-            }catch (\Exception $ex){
-                // content == "Unexpected Error"
-                $response->setContent(json_encode(Result::$FAILURE_EXCEPTION->setContent($ex)));
-                return $response;
-            }
-
-
-        // -- 3 -- Set & Return value
-            $response->setContent(json_encode(Result::$SUCCESS_EMPTY));
-            return $response;
-
     }
 
 
@@ -182,11 +138,13 @@ class UniversityController extends Controller
         $university = $this->getDoctrine()->getRepository('AppBundle:University')->find($universityId);
         $city = $university->getAddress()->getCityId();
         $borough = $university->getAddress()->getBoroughId();
+        $universityMails = $this->getDoctrine()->getRepository('AppBundle:UniversityMail')->findBy(array('university'=>$university));
 
         return $this->render(
             'AppBundle:university:universityUpdate.html.twig',
             array(
                 'university' => $university,
+                'universityMails' => $universityMails,
                 'city' => $city,
                 'borough' => $borough
             )
@@ -219,6 +177,177 @@ class UniversityController extends Controller
 
         // 1.2) Redirect route to university list page
         return $this->redirectToRoute('university_get');
+
+    }
+
+
+    /**
+     * @Route("/delete/mail-server/{universityMailId}", name="university_delete_mailserver")
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function deleteMailServerAction($universityMailId)
+    {
+        // 1) POST OPERATION
+        // 1.1) try to delete university
+        try{
+            $em = $this->getDoctrine()->getManager();
+
+            $universityMail = $em->getRepository('AppBundle:UniversityMail')->find($universityMailId);
+            if (!$universityMail) {
+                throw $this->createNotFoundException(
+                    'No product found for id '.$universityMail
+                );
+            }
+
+            $em->remove($universityMail);
+            $em->flush();
+
+        } catch (Exception $e){}
+
+        // 1.2) Redirect route to university list page
+        return $this->redirectToRoute('university_get');
+
+    }
+
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    //                                          APPLICATION/JSON SERVICES
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    /**
+     * @Route("/add-mail", name="university_add_mailserver")
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function addMailServer(Request $request){
+
+        // -- 1 -- Initialization
+            $response = new Response();
+            $response->headers->set('Content-Type', 'application/json');
+
+        // -- 2 -- Try to Add New Mail Server
+            try{
+
+            // -- 2.1 -- Get borough list
+                $universityId = $request->get('uid');
+                $mailName = $request->get('mailServer');
+
+                $university = $this->getDoctrine()->getRepository('AppBundle:University')->find($universityId);
+                $universityMail = new UniversityMail();
+                $universityMail->setHostname($mailName);
+                $universityMail->setUniversity($university);
+
+                $this->getDoctrine()->getManager()->persist($universityMail);
+                $this->getDoctrine()->getManager()->flush();
+
+                $mail = array();
+                $mail['id'] = $universityMail->getId();
+                $mail['name'] = $universityMail->getHostname();
+
+            // -- 2.2 -- Return Result
+                $response->setContent(json_encode(Result::$SUCCESS->setContent( $mail )));
+                return $response;
+
+            }catch (\Exception $ex){
+                // content == "Unexpected Error"
+                $response->setContent(json_encode(Result::$FAILURE_EXCEPTION->setContent($ex)));
+                return $response;
+            }
+
+        // -- 3 -- Set & Return value
+            $response->setContent(json_encode(Result::$SUCCESS_EMPTY));
+            return $response;
+
+    }
+
+    /**
+     * @Route("/remove-mail", name="university_remove_mailserver")
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function removeMailServer(Request $request){
+
+        // -- 1 -- Initialization
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+
+        // -- 2 -- Try to Add New Mail Server
+        try{
+
+            // -- 2.1 -- Get borough list
+            $universityId = $request->get('uid');
+            $universityMailId = $request->get('mid');
+
+            $em = $this->getDoctrine()->getManager();
+
+            $universityMail = $em->getRepository('AppBundle:UniversityMail')->find($universityMailId);
+            if (!$universityMail) {
+                throw $this->createNotFoundException(
+                    'No product found for id '.$universityMail
+                );
+            }
+
+            $em->remove($universityMail);
+            $em->flush();
+
+            // -- 2.2 -- Return Result
+            $response->setContent(json_encode(Result::$SUCCESS->setContent( 'University mail server removed!' )));
+            return $response;
+
+        }catch (\Exception $ex){
+            // content == "Unexpected Error"
+            $response->setContent(json_encode(Result::$FAILURE_EXCEPTION->setContent($ex)));
+            return $response;
+        }
+
+        // -- 3 -- Set & Return value
+        $response->setContent(json_encode(Result::$SUCCESS_EMPTY));
+        return $response;
+
+    }
+
+
+
+    /**
+     * @Route("/borough-list", name="university_borough_list")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function getBoroughAction(Request $request)
+    {
+
+        // -- 1 -- Initialization
+            $response = new Response();
+            $response->headers->set('Content-Type', 'application/json');
+
+        // -- 2 -- Try to Get Borough List
+            try{
+
+            // -- 2.1 -- Get borough list
+                $city_id = $request->get('cid');
+                $boroughList = $this->getDoctrine()
+                    ->getRepository('AppBundle:Borough')
+                    ->findBy(array('cityId'=>$city_id));
+
+                $boroughResponse = array();
+                for($i=0; $i<count($boroughList);$i++){
+                    $borough['borough_name'] = $boroughList[$i]->getName();
+                    $borough['borough_id'] = $boroughList[$i]->getId();
+                    array_push($boroughResponse,$borough);
+                }
+
+            // -- 2.1 -- Return Result
+                $response->setContent(json_encode(Result::$SUCCESS->setContent( $boroughResponse )));
+                return $response;
+
+            }catch (\Exception $ex){
+                // content == "Unexpected Error"
+                $response->setContent(json_encode(Result::$FAILURE_EXCEPTION->setContent($ex)));
+                return $response;
+            }
+
+
+        // -- 3 -- Set & Return value
+            $response->setContent(json_encode(Result::$SUCCESS_EMPTY));
+            return $response;
 
     }
 
