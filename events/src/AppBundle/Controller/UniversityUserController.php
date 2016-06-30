2a1ebbe45f90abfc6a58790d10be3fe53f91966b
kemalsami <kemalsami.karaca@tubitak.gov.tr>
2016-06-30 13:05:37 +0300

    ** Guppy-ADMIN-5-Assing admin to clubs ** users can remove university mail addresses

--- events/src/AppBundle/Controller/UniversityUserController.php
+++ events/src/AppBundle/Controller/UniversityUserController.php
@@ -36,4 +36,31 @@ class UniversityUserController extends Controller
         );
     }
 
+
+    /**
+     * @Route("/remove/{universityUserId}", name="university_user_delete")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function removeUniversityUserAction($universityUserId){
+        // 1) POST OPERATION
+        // 1.1) try to delete university
+        try{
+            $em = $this->getDoctrine()->getManager();
+
+            $universityUser = $em->getRepository('AppBundle:UniversityUser')->find($universityUserId);
+            if (!$universityUser) {
+                throw $this->createNotFoundException(
+                    'No product found for id '.$universityUser
+                );
+            }
+
+            $em->remove($universityUser);
+            $em->flush();
+
+        } catch (Exception $e){}
+
+        // 1.2) Redirect route to university list page
+        return $this->redirectToRoute('settings_emails');
+    }
+
 }
