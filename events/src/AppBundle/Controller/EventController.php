7785aefbd6e63d47edf99b44a41b98af7914ceed
kemalsami <kemalsami.karaca@tubitak.gov.tr>
2016-12-04 00:00:56 +0300

    Guppy-ADMIN-6-Events CRUD operations - Event List feature done

--- events/src/AppBundle/Controller/EventController.php
+++ events/src/AppBundle/Controller/EventController.php
@@ -29,13 +29,59 @@ class EventController extends Controller
     public function listAction(Request $request)
     {
 
+        $selectedCommunityId = null;
+        $selectedUniversityId = null;
+        $communityList = array();
+
         $eventList = $this->getDoctrine()->getRepository('AppBundle:Event')->findAll();
+        $universityList = $this->getDoctrine()->getRepository('AppBundle:University')->findAll();
+
+        // 1) POST OPERATION
+        if($request->getMethod() == 'POST'){
+            $em = $this->getDoctrine()->getManager();
+
+            try{
+                $selectedCommunityId = $request->get('community_id');
+                $selectedUniversityId = $request->get('university_id');
+
+                // eğer kulüp id değeri varsa o kulübün etkinlik listesi getir
+                if($selectedCommunityId && $this->getDoctrine()->getRepository('AppBundle:Community')->find($request->get('community_id'))){
+                    $communityList = $this->getDoctrine()->getRepository('AppBundle:Community')->findBy(array('university'=>$request->get('university_id')));
+                    $eventList = $this->getDoctrine()->getRepository('AppBundle:Event')->findEventsByCommunityId($request->get('community_id'));
+
+
+                // eğer universite id değeri varsa o universitenin etkinlik listesi getir
+                }else if($selectedUniversityId && $this->getDoctrine()->getRepository('AppBundle:University')->find($request->get('university_id'))){
+                    $communityList = $this->getDoctrine()->getRepository('AppBundle:Community')->findBy(array('university'=>$request->get('university_id')));
+                    $communityIdList = array();
+                    for($i=0; $i<count($communityList);$i++){
+                        array_push($communityIdList ,$communityList[$i]->getId());
+                    }
+                    $eventList = $this->getDoctrine()->getRepository('AppBundle:Event')->findEventsByCommunityIdList($communityIdList);
+
+
+                // hiçbiri yoksa tüm etkinleri getir
+                }else{
+                    $eventList = $this->getDoctrine()->getRepository('AppBundle:Event')->findAll();
+                }
+
+                $universityList = $em->getRepository('AppBundle:University')->findAll();
+            } catch (Exception $e){
+                
+            }
+        }
 
         return $this->render( 'AppBundle:event:eventList.html.twig', array(
-                'eventList'=>$eventList
+                'eventList'=>$eventList,
+                'universityList'=>$universityList,
+                'communityList'=>$communityList,
+                'selectedCommunityId'=>$selectedCommunityId,
+                'selectedUniversityId'=>$selectedUniversityId
             )
         );
     }
+
+
 
     /**
      * @Route("/post", name="event_post")
@@ -101,80 +147,80 @@ class EventController extends Controller
         );
     }
 
-    /**
-     * @Route("/put/{communityId}", name="community_put")
-     * @Security("has_role('ROLE_USER')")
-     */
-    public function putAction(Request $request , $communityId)
-    {
+//    /**
+//     * @Route("/put/{communityId}", name="community_put")
+//     * @Security("has_role('ROLE_USER')")
+//     */
+//    public function putAction(Request $request , $communityId)
+//    {
+//
+//        // 1) POST OPERATION
+//        if($request->getMethod() == 'POST'){
+//
+//            // 1.1) try to update community
+//            try{
+//                $em = $this->getDoctrine()->getManager();
+//
+//                // 1.1.1) University Repository should try to register university
+//                $community = $em->getRepository('AppBundle:Community')->find($communityId);
+//
+//                // 1.1.2) Check University exist
+//                if (!$community) {
+//                    throw $this->createNotFoundException(
+//                        'No product found for id '.$communityId
+//                    );
+//                }
+//
+//                // 1.1.3) Update
+//                $community->setName( $request->get('community_name') );
+//                $community->setDescription( $request->get('community_description') );
+//                $em->flush();
+//
+//            } catch (Exception $e){}
+//
+//            // 1.2) Redirect route to university list page
+//            return $this->redirectToRoute('community_list');
+//        }
+//
+//        // 2) DEFAULT CASE
+//        $community = $this->getDoctrine()->getRepository('AppBundle:Community')->find($communityId);
+//
+//        return $this->render(
+//            'AppBundle:community:communityUpdate.html.twig', array(
+//                'community' => $community,
+//            )
+//        );
+//    }
 
-        // 1) POST OPERATION
-        if($request->getMethod() == 'POST'){
 
-            // 1.1) try to update community
-            try{
-                $em = $this->getDoctrine()->getManager();
-
-                // 1.1.1) University Repository should try to register university
-                $community = $em->getRepository('AppBundle:Community')->find($communityId);
-
-                // 1.1.2) Check University exist
-                if (!$community) {
-                    throw $this->createNotFoundException(
-                        'No product found for id '.$communityId
-                    );
-                }
-
-                // 1.1.3) Update
-                $community->setName( $request->get('community_name') );
-                $community->setDescription( $request->get('community_description') );
-                $em->flush();
-
-            } catch (Exception $e){}
-
-            // 1.2) Redirect route to university list page
-            return $this->redirectToRoute('community_list');
-        }
-
-        // 2) DEFAULT CASE
-        $community = $this->getDoctrine()->getRepository('AppBundle:Community')->find($communityId);
-
-        return $this->render(
-            'AppBundle:community:communityUpdate.html.twig', array(
-                'community' => $community,
-            )
-        );
-    }
-
-
-    /**
-     * @Route("/delete/{communityId}", name="community_delete")
-     * @Security("has_role('ROLE_USER')")
-     */
-    public function deleteAction($communityId)
-    {
-
-        // 1) POST OPERATION
-        // 1.1) try to delete community
-        try{
-            $em = $this->getDoctrine()->getManager();
-
-            $community = $em->getRepository('AppBundle:Community')->find($communityId);
-            if (!$community) {
-                throw $this->createNotFoundException(
-                    'No product found for id '.$communityId
-                );
-            }
-
-            $em->remove($community);
-            $em->flush();
-
-        } catch (Exception $e){}
-
-        // 1.2) Redirect route to community list page
-        return $this->redirectToRoute('community_list');
-
-    }
+//    /**
+//     * @Route("/delete/{communityId}", name="community_delete")
+//     * @Security("has_role('ROLE_USER')")
+//     */
+//    public function deleteAction($communityId)
+//    {
+//
+//        // 1) POST OPERATION
+//        // 1.1) try to delete community
+//        try{
+//            $em = $this->getDoctrine()->getManager();
+//
+//            $community = $em->getRepository('AppBundle:Community')->find($communityId);
+//            if (!$community) {
+//                throw $this->createNotFoundException(
+//                    'No product found for id '.$communityId
+//                );
+//            }
+//
+//            $em->remove($community);
+//            $em->flush();
+//
+//        } catch (Exception $e){}
+//
+//        // 1.2) Redirect route to community list page
+//        return $this->redirectToRoute('community_list');
+//
+//    }
 
 
     /*******************************************************************************************************************
--- events/src/AppBundle/Repository/EventRepository.php
+++ events/src/AppBundle/Repository/EventRepository.php
@@ -11,4 +11,44 @@ use Doctrine\ORM\EntityRepository;
  */
 class EventRepository extends EntityRepository
 {
+
+    /**
+     * Loads the user for the given username.
+     * This method must return null if the user is not found.
+     *
+     * @param string $username The username
+     * @return UserInterface|null
+     */
+    public function findEventsByCommunityId($communityId)
+    {
+        // TODO: Implement loadUserByUsername() method.
+        return $this->createQueryBuilder('event')
+            ->join('event.communityUser', 'communityUser')
+            ->join('communityUser.community', 'community')
+            ->where('community.id = :communityId')
+            ->setParameter('communityId', intval($communityId))
+            ->getQuery()
+            ->getResult();
+    }
+
+
+    /**
+     * Loads the user for the given username.
+     * This method must return null if the user is not found.
+     *
+     * @param string $username The username
+     * @return UserInterface|null
+     */
+    public function findEventsByCommunityIdList($communityIdList)
+    {
+        // TODO: Implement loadUserByUsername() method.
+        return $this->createQueryBuilder('event')
+            ->join('event.communityUser', 'communityUser')
+            ->innerJoin('communityUser.community', 'community')
+            ->where('community.id IN (:communityIdList)')
+            ->setParameter('communityIdList', $communityIdList)
+            ->getQuery()
+            ->getResult();
+    }
+
 }
