d5cfa52a007a50fb8f5084ff0e7b6b1f3a0860a1
kemalsami <kemalsami.karaca@tubitak.gov.tr>
2016-12-21 10:34:50 +0300

    Guppy-Event v.1.0.7.2 - Events' times and links are updated

--- events/src/AppBundle/Controller/DefaultController.php
+++ events/src/AppBundle/Controller/DefaultController.php
@@ -66,11 +66,11 @@ class DefaultController extends Controller
 
         $weekStartDate = new \DateTime();
         $weekStartDate->setTime(0,0);
-        $weekStartDate->sub(new \DateInterval("P3D"));
+        //$weekStartDate->sub(new \DateInterval("P3D"));
 
         $weekFinishDate = new \DateTime();
         $weekFinishDate->setTime(0,0);
-        $weekFinishDate->add(new \DateInterval("P3D"));
+        $weekFinishDate->add(new \DateInterval("P6D"));
 
         $eventList = $this->getDoctrine()->getRepository('AppBundle:Event')->findEventsByDate( $weekStartDate,$weekFinishDate);
         
