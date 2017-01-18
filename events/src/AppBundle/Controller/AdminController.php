ae1ef6d64e19cd4b129f79e04af2a7bc693c08b8
kemalsami <kemalsami.karaca@tubitak.gov.tr>
2017-01-18 14:53:33 +0300

    #39 - Guppy v.1.0.7.28 - Admin Panel Community User İşlemleri Düzeltilmesi - Admin panel topluluk kullanıcıları listelenmesi ve onaylanması düzenlendi - CommunityUserRoles tablosuna kullanıcıların reject edilmesi durumları için description eklendi

--- events/src/AppBundle/Controller/AdminController.php
+++ events/src/AppBundle/Controller/AdminController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Result;
 
 /**
  * @Route("/admin", name="admin")
@@ -48,11 +50,93 @@ class AdminController extends Controller
     {
         $data = array();
 
-        $communityUserList = $this->getDoctrine()->getRepository('AppBundle:CommunityUser')->findAllExceptAdmin();
 
-        $data['communityUserList'] = $communityUserList;
+        $pendingState = $this->getDoctrine()->getRepository('AppBundle:CommunityUserRoleState')->findPendingState();
+        $acceptState = $this->getDoctrine()->getRepository('AppBundle:CommunityUserRoleState')->findAcceptState();
+        $rejectState = $this->getDoctrine()->getRepository('AppBundle:CommunityUserRoleState')->findRejectState();
+        $communityUsers = $this->getDoctrine()->getRepository('AppBundle:CommunityUser')->findAll();
+        $communityUserRoles = $this->getDoctrine()->getRepository('AppBundle:CommunityUserRoles')->findBy(array('communityUser'=>$communityUsers ));
+
+        $data['statePending'] = $pendingState;
+        $data['stateAccept'] = $acceptState;
+        $data['stateReject'] = $rejectState;
+        $data['communityUserRoles'] = $communityUserRoles;
 
         return $this->render('AppBundle:admin:communityuser_list.html.twig', $data);
+    }
+
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    //                                          APPLICATION/JSON SERVICES
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    /**
+     * @Route("community/applications/confirm", name="admin_user_community_membership_applications_confirm")
+     */
+    public function communityMembershipApplicationsConfirmAction(Request $request)
+    {
+        // -- 1 -- Initialization
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $data = array();
+
+        // -- 2 --
+        try{
+
+            // -- 2.1 -- Get parameters
+            $operation = $request->get('operation');
+            $communityUserRoleId = $request->get('communityUserRoleId');
+
+            $communityUserRole = $this->getDoctrine()->getRepository('AppBundle:CommunityUserRoles')->find($communityUserRoleId);
+
+            if (!$communityUserRole) {
+                $response->setContent(json_encode(Result::$FAILURE_EXCEPTION->setContent('Community User Role bulunamadi')));
+                return $response;
+            }
+            $pendingState = $this->getDoctrine()->getRepository('AppBundle:CommunityUserRoleState')->findPendingState();
+            if ($communityUserRole->getState() != $pendingState) {
+                $response->setContent(json_encode(Result::$FAILURE_EXCEPTION->setContent('Uye durumu uygun degil')));
+                return $response;
+            }
+
+            $acceptState = $this->getDoctrine()->getRepository('AppBundle:CommunityUserRoleState')->findAcceptState();
+            $rejectState = $this->getDoctrine()->getRepository('AppBundle:CommunityUserRoleState')->findRejectState();
+            switch ($operation){
+                case 'confirm':
+                    $communityUserRole->setState($acceptState);
+                    $data['success_msg'] = 'Üye başarıyla onaylandı';
+                    $data['roleState'] = 'accepted';
+                    break;
+                case 'reject':
+                    $communityUserRole->setState($rejectState);
+                    $data['success_msg'] = 'Üye başarıyla reddedildi';
+                    $data['roleState'] = 'rejected';
+                    break;
+                default:
+                    break;
+            }
+
+            $communityUserRole->setUpdateDate(new \DateTime('now'));
+            $communityUserRole->setPerformBy($this->getUser());
+
+            $this->getDoctrine()->getManager()->persist($communityUserRole);
+            $this->getDoctrine()->getManager()->flush();
+
+
+            // -- 2.2 -- Return Result
+            $response->setContent(json_encode(Result::$SUCCESS->setContent($data)));
+            return $response;
+
+        }catch (\Exception $ex){
+            // content == "Unexpected Error"
+            $response->setContent(json_encode(Result::$FAILURE_EXCEPTION->setContent($ex->getMessage())));
+            return $response;
+        }
+
+        // -- 3 -- Set & Return value
+        $response->setContent(json_encode(Result::$SUCCESS_EMPTY));
+        return $response;
     }
     
 }
